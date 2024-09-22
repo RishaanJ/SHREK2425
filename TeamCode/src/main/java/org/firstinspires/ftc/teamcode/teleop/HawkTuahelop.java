dfa4b25daa3ba3fc74914d3238fd39fd7a9d97d9
RishaanJ <rishaanjain@gmail.com>
2024-09-22 11:12:35 -0700

    tuah tuah tuah

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/HawkTuahelop.java
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.teamcode.teleop;
+
+import com.qualcomm.hardware.limelightvision.Limelight3A;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public class HawkTuahelop extends LinearOpMode {
+    private Limelight3A limelight;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        limelight = hardwareMap.get(Limelight3A.class, "limelight");
+
+        telemetry.setMsTransmissionInterval(11);
+
+        limelight.pipelineSwitch(0);
+
+        /*
+         * Starts polling for data.
+         */
+        limelight.start();
+    }
+}
