66486090841a8c48d7aa670ec3dbf720c966ff78
RishaanJ <rishaanjain@gmail.com>
2024-09-22 11:34:05 -0700

    Hawk 2: Ahh

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/HawkTuahelop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/HawkTuahelop.java
@@ -1,11 +1,15 @@
 package org.firstinspires.ftc.teamcode.teleop;
 
+import com.qualcomm.hardware.limelightvision.LLResult;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
+
 public class HawkTuahelop extends LinearOpMode {
     private Limelight3A limelight;
-
+    private LLResult result;
+    Pose3D botpose = result.getBotpose();
     @Override
     public void runOpMode() throws InterruptedException {
         limelight = hardwareMap.get(Limelight3A.class, "limelight");
