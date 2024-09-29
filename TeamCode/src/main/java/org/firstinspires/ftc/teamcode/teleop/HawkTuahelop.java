f35302d4fb8f9afa5a5fab43818cce8a90370fab
RishaanJ <rishaanjain@gmail.com>
2024-09-29 11:01:28 -0700

    what did hawk 1 say to hawk 2? uhhhh

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/HawkTuahelop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/HawkTuahelop.java
@@ -1,26 +1,38 @@
 package org.firstinspires.ftc.teamcode.teleop;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.hardware.limelightvision.LLResult;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
 
+@TeleOp(name = "HawkTuaheleop")
+@Config
 public class HawkTuahelop extends LinearOpMode {
     private Limelight3A limelight;
-    private LLResult result;
-    Pose3D botpose = result.getBotpose();
+
     @Override
     public void runOpMode() throws InterruptedException {
         limelight = hardwareMap.get(Limelight3A.class, "limelight");
-
         telemetry.setMsTransmissionInterval(11);
-
         limelight.pipelineSwitch(0);
 
-        /*
-         * Starts polling for data.
-         */
+        // Add waitForStart() to wait for the Start button to be pressed
+        waitForStart();
+
         limelight.start();
+        while (opModeIsActive()) {
+            LLResult result = limelight.getLatestResult();
+            if (result != null) {
+                if (result.isValid()) {
+                    Pose3D botpose = result.getBotpose();
+                    telemetry.addData("tx", result.getTx());
+                    telemetry.addData("ty", result.getTy());
+                    telemetry.addData("Botpose", botpose.toString());
+                }
+            }
+            telemetry.update();
+        }
     }
 }
